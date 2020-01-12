6e9863d1768ab200689740f14f30d4ccdd9704ae
atorov <veselin.lkp@gmail.com>
2020-01-12 23:21:39 +0200

    connecting be to mongodb

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 // // const uuidv4 = require('uuid/v4')
+
+const xsettings = require('./private/xsettings')
 
 const HTTPError = require('./models/http-error')
 
@@ -27,21 +30,22 @@ app.use((err, _, res, next) => {
 
     res.status(err.code || 500)
     return res.json({ message: err.message || 'An unknown error ocurred!' })
-})
+});
 
-// const DUMMY_PRODUCTS = []
+(async () => {
+    try {
+        // TODO: remove the password from the repo
+        await mongoose.connect(
+            `mongodb+srv://${xsettings.mongodb.user}:${xsettings.mongodb.password}@cluster0-pp2gz.mongodb.net/mern_stack_demo?retryWrites=true&w=majority`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+        )
+    }
+    catch (error) {
+        console.error('::: [db/connect] Error:', error)
+    }
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-//     )
-//     res.setHeader(
-//         'Access-Control-Allow-Methods',
-//         'GET, POST, PATCH, DELETE, OPTIONS',
-//     )
-//     next()
-// })
-
-app.listen(5000)
+    app.listen(5000)
+})()
